5f387f3680e470bf879c312dc9c9b3976f4fe7f4
amkosh <amkosh@mail.ru>
2022-05-26 12:45:53 +0500

    release 0.0.1

--- js/script.js
+++ js/script.js
@@ -1,35 +1,72 @@
 const alphabet = 'АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ';
 const nums = '0123456789';
 const task = document.getElementById('task');
-
+const stack = document.getElementById('stack');
+let word = '';
+let cursor = 0;
 taskLoad();
 
 //Заполняем задание
 function taskLoad(){
-  let word = words[getRandomInt(0, words.length-1)];
+  word = words[getRandomInt(0, words.length-1)];
   task.innerHTML = '';
+  stack.innerHTML = '';
 
   for(i = 0; i < word.length; i++){
     let letter = word[i];
     let element = document.createElement('p');
     element.className = 'unsolved';
+    element.id = 'taskCard';
     element.innerText = letter;
     task.appendChild(element);
   }
+  drawCards();
 }
 
 //Рисуем карточки
-for(i = 1; i <= 20; i++){
-  let card = document.createElement('div');
-  card.className = 'card';
-  card.innerText = i;
-  card.id = i;
-  document.getElementById('stack').appendChild(card);
-  card.addEventListener('click', press);
+function drawCards(){
+  let cardWord = Array.from(word);
+  let c = (20-cardWord.length);
+
+  //Добавляем дополнительные буквы
+  for(i = 0; i < c; i++){
+    cardWord.push(alphabet[getRandomInt(0,32)]);
+  }
+  cardWord = shuffle(cardWord);
+
+  //Рисуем и добавляем карточки
+  for(i = 0; i < 20; i++){
+    let card = document.createElement('div');
+    card.className = 'card';
+    card.innerText = cardWord[i];
+    card.id = 'card';
+    document.getElementById('stack').appendChild(card);
+    card.addEventListener('click', press);
+  }
+
 }
 
+//Обрабатываем нажатие
 function press(event){
-  document.getElementById('task').innerText = event.target.id;
+  for(card of document.querySelectorAll('#card')){
+    card.className = 'card';
+  }
+
+  let pressed = event.target.innerText;
+  let curTask = task.querySelectorAll('#taskCard');
+
+  if(pressed == curTask[cursor].innerText){
+    curTask[cursor].className = 'solved';
+    //event.target.remove();
+    event.target.style.visibility = 'hidden';
+    cursor++;
+  } else {
+    event.target.className = 'card__wrong';
+  }
+  if(cursor == curTask.length){
+    cursor = 0;
+    taskLoad();
+  }
 }
 
 //Рандомное целое число
@@ -37,4 +74,18 @@ function getRandomInt(min, max) {
   min = Math.ceil(min);
   max = Math.floor(max);
   return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+//Перемешивание массива
+function shuffle(array) {
+  let currentIndex = array.length,  randomIndex;
+
+  while (currentIndex != 0) {
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex--;
+
+    [array[currentIndex], array[randomIndex]] = [
+      array[randomIndex], array[currentIndex]];
+  }
+  return array;
 }
